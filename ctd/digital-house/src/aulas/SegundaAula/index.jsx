9864187baec1de1597fce095d3ab628836c077ab
Julio Campos <juliojf89@gmail.com>
2022-10-20 19:16:49 -0300

    segunda aula

---
+++ ctd/digital-house/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,15 @@
+import './style'
+export function SegundaAula (){
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto Escolhido: Netflix</h2>
+            <ul>
+                <li>
+                    <h1>Primeiro Componente</h1>
+                    <p></p>
+                </li>
+            </ul>
+        </main>
+    )
+}
